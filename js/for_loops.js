4d576aaba83a8b57662ccf188643ecd6e9e08df3
lening rodas <lening.a.rodas@gmail.com>
2020-10-05 12:26:42 -0500

    loops exercise review/work

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,5 +1,4 @@
-
-// verify this with another succesfull code from fellow classmate
+ //verify this with another succesfull code from fellow classmate
 // var multiplier = 9;
 // for (var i = 0; i <= 10; i++) {
 //     var result = multiplier * i;
@@ -7,16 +6,29 @@
 // }
 
 
-// for (var i = 0; i <= 10; i++) {
+//
+// for (var i = 0; i < 10; i++) {
 //     var randomNumber= Math.floor((Math.random() * (200 - 20 + 1)) + 20);
-//     if(i % 2 === 0){
+//     if(i % 2 === 0) {
 //         console.log(randomNumber + " number is even");
-//     if(i % 3 === 0){
+//     } else { (i % 3 !== 0)
 //         console.log(randomNumber + " number is odd");
 //     }
-//     }
-// }
 //
+// }
+
+/* walkthough
+    for(var i = 0; i , 10; i++){
+    var randomNUmber = math.floor((math.random() * 180) + 20;
+    if(randomNumber % 2==0){
+    console.log(randomNumber + " number is even");
+    } else{
+    console.log(randomNumber + " is odd")
+ */
+
+
+
+
 //
 // function generatePyramid(){
 //     var rows = 10;
@@ -30,10 +42,29 @@
 //         }
 // }
 // console.log(generatePyramid());
+// //
 
-// for (var Num = 100; Num > 0; Num--){
+/* walkthough
+     for (var i = 1; i <= 9; i++) {
+   var = output = " ";
+       for (var j = 1; j <= i; j++) {
+       output += outer;
+       }
+       console.log(output);
+       }
+
+
+       **solution 2**
+        for(var i = 1; i <= 9; i++){
+        console.log(i.toString().repeat(i));
+       }
+
+  */
+
+
+ // for (var Num = 100; Num > 0; Num--){
 //     if(Num % 5 === 0){
 //         console.log(Num);
 //     }
 // }
-
+//
