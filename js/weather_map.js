c8f88a316e470508aa1d04ebe14d1a183c226e73
lening rodas <lening.a.rodas@gmail.com>
2020-11-20 08:41:59 -0600

    work in progress

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    "use strict";
+
 
     mapboxgl.accessToken = mapboxToken;
 
@@ -20,13 +20,66 @@ $(document).ready(function () {
         .setDraggable(true)
         .addTo(map);
 
-    $.get("http://api.openweathermap.org/data/2.5/weather", {
-            APPID: OPEN_WEATHER_APPID,
-            q: "San Antonio, US",
-            units: "imperial"
-        }).done(function (data) {
-            console.log(data);
-        });
+    // $.get("http://api.openweathermap.org/data/2.5/weather", {
+    //         APPID: OPEN_WEATHER_APPID,
+    //         q: "london, uk",
+    //         units: "imperial"
+    //     }).done(function (data) {
+    //         console.log(data);
+    //     });
+
+    $('#submitWeather').click(function() {
+        var city = $("#userInput").val();
+        if (city != '') {
+            $.get("http://api.openweathermap.org/data/2.5/weather", {
+                APPID: OPEN_WEATHER_APPID,
+                q: city,
+                units: "imperial"
+            }).done( function (data) {
+                    var widget = show(data);
+
+                    $("#map").html(widget);
+
+                    $("#city").val('');
+                    console.log(data);
+                }
+            );
+        } else {
+            $('#error').html('Field cannot be empty');
+        }
+
+    });
+
+
+
+        function show(data){
+            var html = '<div id="weathercard" class="card-deck" style="background-color: black; color: whitesmoke; border: 5px solid">';
+                html +='<h3 class="card-header justify-content-center"><strong>Weather</strong></h3>'
+                html += "<li><strong>Temperature</strong>: " + data.main.temp + "</li>"
+                html += "<li><strong>Humidity</strong>: " + data.main.humidity + "</li>"
+                html += "<li class='justify-content-center'><strong>Pressure</strong>: " + data.main.pressure + "</li>"
+                html += "<li><strong>Feels like</strong>: " + data.main.feels_like + "</li>"
+                html +='</div>';
+
+                $('#weathercard').append(html);
+
+            }
+
+
+
+
+
+
+    //
+    // function onDragEnd() {
+    //     var lngLat = dallasLocation.getLngLat();
+    //     coordinates.style.display = 'block';
+    //     coordinates.innerHTML =
+    //         'Longitude: ' + lngLat.lng + '<br />Latitude: ' + lngLat.lat;
+    //     console.log(lngLat)
+    // }
+    //
+    // dallasLocation.on('dragend', onDragEnd);
 
      });
 
@@ -40,13 +93,4 @@ $(document).ready(function () {
 
 
 
-//
-// function onDragEnd() {
-//     var lngLat = dallasLocation.getLngLat();
-//     coordinates.style.display = 'block';
-//     coordinates.innerHTML =
-//         'Longitude: ' + lngLat.lng + '<br />Latitude: ' + lngLat.lat;
-//     console.log(lngLat)
-// }
-//
-// dallasLocation.on('dragend', onDragEnd);
+
